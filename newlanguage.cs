2c7ff644908744177337b2bbe4cd646efebb336a
DIRGH712 <>
2021-03-29 03:39:54 +0530

    file add

---
+++ newlanguage.cs
@@ -0,0 +1,10 @@
+using System;
+namespace newlanguage
+{
+    class Hello {         
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
